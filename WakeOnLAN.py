b2c06f47e3e59910672be8115f2b80c982978c5d
gravelit <graveline17@gmail.com>
2020-11-27 09:23:08 -0700

    Initial commit

---
+++ WakeOnLAN.py
@@ -0,0 +1,27 @@
+import argparse
+import configparser
+import re
+
+from wakeonlan import send_magic_packet
+
+parser = argparse.ArgumentParser(description='Calculates the cost of a deck using the Scryfall API')
+parser.add_argument(dest='computer', nargs='?', default=None, help='Computer to wake')
+args = parser.parse_args()
+
+config = configparser.ConfigParser()
+config.read('wake.ini')
+
+mac_pattern = r'\w{2}-\w{2}-\w{2}-\w{2}-\w{2}-\w{2}'
+
+computer = args.computer
+mac = None
+if computer:
+    if computer.lower() in config:
+        mac = config[computer.lower()]['mac']
+        print('Waking {}...'.format(computer))
+    elif re.match(mac_pattern, computer):
+        mac = computer
+        print('Waking {}...'.format(mac))
+
+    if mac:
+        send_magic_packet(mac)
